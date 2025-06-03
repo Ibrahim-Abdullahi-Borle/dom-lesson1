11b31a857dd0cb0e260f6deb404bdcefc2962b5d
Ibrahim-Abdullahi-Borle <ibroabdullahi254@gmail.com>
2025-06-03 02:46:00 -0700

    dom-lesson1

---
+++ index.js
@@ -0,0 +1,30 @@
+//DOM document object model
+// 1. getElementByTagName
+console.log(document.getElementsByTagName("h1"));
+//2. getElementById
+console.log(document.getElementById("main"));
+
+// 3. getElementsByClassName
+console.log(document.getElementsByClassName("cls"));
+
+
+// classwork
+console.log(document.getElementsByTagName("button"))
+
+
+
+console.log(document.getElementsByClassName("greet"));
+
+console.log(document.getElementById("hi"));
+
+// 4. querySelector
+
+console.log(document.querySelector("h1"))
+
+console.log(document.querySelector("#main"))
+
+console.log(document.querySelector(".cls-1"));
+
+
+const h1 = document.querySelector(".cls");
+console.log(h1)
